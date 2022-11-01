b873f71b3ba9a542d3dde7f264fa3a876e51ca70
Vladimir Antipov <AntipovVla02@sbsit.com>
2022-11-01 22:59:32 +0300

    insert calibration

--- db/insertData.js
+++ db/insertData.js
@@ -1,13 +1,10 @@
 const { pool } = require('./db');
 
-async function insertData(data) {
-    if (data.fPort === 2) {
-        let data_hex = new Buffer(data.data, 'base64').toString('binary');
-        console.log(data_hex);
-
+async function insertData(payload) {
+    if (payload.fPort === 2) {
         await pool.query(
-            'INSERT INTO raw_data(deveui, data_hex, payload) VALUES ($1, $2, $3)',
-            [data.devEUI, data.data, data]
+            'INSERT INTO raw_data(deveui, data_base64, payload) VALUES ($1, $2, $3)',
+            [payload.devEUI, payload.data, payload]
         ).then(res => console.log(`Inserted ${res.rowCount} rows`));
     }
 }
