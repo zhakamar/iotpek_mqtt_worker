466c4ccb86baecd01e690e92290934919b5d6751
Vladimir Antipov <AntipovVla02@sbsit.com>
2022-11-01 21:33:04 +0300

    insertion test

--- db/db.js
+++ db/db.js
@@ -1,11 +1,15 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+const config = {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DATABASE,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-});
+}
+
+console.log(config);
+
+const pool = new Pool(config);
 
 module.exports = { pool };
