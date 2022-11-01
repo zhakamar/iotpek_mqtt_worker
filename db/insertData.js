51792bcc92ef63e9ea292f5c5648f8672f762ae0
Vladimir Antipov <AntipovVla02@sbsit.com>
2022-11-01 22:37:54 +0300

    insertion test

--- db/insertData.js
+++ db/insertData.js
@@ -2,6 +2,9 @@ const { pool } = require('./db');
 
 async function insertData(data) {
     if (data.fPort === 2) {
+        let data_hex = new Buffer(data.data, 'base64').toString('binary');
+        console.log(data_hex);
+
         await pool.query(
             'INSERT INTO raw_data(deveui, data_hex, payload) VALUES ($1, $2, $3)',
             [data.devEUI, data.data, data]
