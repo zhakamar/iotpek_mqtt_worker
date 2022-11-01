d8eec78f170210cc9b6668440d0785768de1e2af
Vladimir Antipov <AntipovVla02@sbsit.com>
2022-11-01 21:24:59 +0300

    insertion test

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: process.env.HOST,
-    port: process.env.PORT,
-    database: process.env.DATABASE,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DATABASE,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
 });
 
 module.exports = { pool };
--- db/insertData.js
+++ db/insertData.js
@@ -1,11 +1,12 @@
 const { pool } = require('./db');
 
 async function insertData(data) {
-    const res = await pool.query(
+    const query = await pool.query(
         'INSERT INTO raw_data(deveui, data_hex, payload) VALUES ($1, $2, $3)',
         [data.devEUI, data.data, data]
     );
-    console.log('SQL result:', res);
+
+    query.then(res => console.log('SQL result:', res));
 }
 
 module.exports = { insertData };
