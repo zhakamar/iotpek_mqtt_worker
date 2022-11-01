3f679e5d5168a98eb577561ea066c385f3fd1698
Vladimir Antipov <AntipovVla02@sbsit.com>
2022-11-01 22:17:49 +0300

    insertion test

--- db/insertData.js
+++ db/insertData.js
@@ -3,12 +3,10 @@ const { pool } = require('./db');
 async function insertData(data) {
     console.log(data);
 
-    const query = await pool.query(
+    await pool.query(
         'INSERT INTO raw_data(deveui, data_hex, payload) VALUES ($1, $2, $3)',
         [data.devEUI, data.data, data]
-    );
-
-    query.then(res => console.log('SQL result:', res));
+    ).then(res => console.log('SQL result:', res));
 }
 
 module.exports = { insertData };
