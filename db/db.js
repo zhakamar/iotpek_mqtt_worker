e87d20638beacf3a227f0d69cfa7163e68ef0caa
Vladimir Antipov <AntipovVla02@sbsit.com>
2022-11-01 21:56:43 +0300

    insertion test

--- db/db.js
+++ db/db.js
@@ -3,17 +3,12 @@ const { Pool } = require('pg');
 
 dotenv.config({ path: '.env' });
 
-const config = {
+const pool = new Pool({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DATABASE,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-}
-
-console.log(config);
-console.log(process.env);
-
-const pool = new Pool(config);
+});
 
 module.exports = { pool };
